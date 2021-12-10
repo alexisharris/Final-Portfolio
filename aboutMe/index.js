57aafaa7193ad8ad8da2c587086ec406d2419f57
alexisharris <alha7603@colorado.edu>
2021-12-10 12:49:34 -0700

    Updating Button on About Page

---
+++ aboutMe/index.js
@@ -0,0 +1,3 @@
+document.getElementById("frontAboutButton").addEventListener("click", function() {
+        location.href = "https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:0e4901e8-de3f-49a0-b7de-ba05ad5547c3";
+    });
